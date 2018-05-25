469307332c67ef37c89ec9107c96d3390db8086a
farid.kazemi63 <farid.kazemi63@gmail.com>
2018-05-25 12:37:51 +1000

    First commit for webApiToken

---
+++ WebApplication2/WebApplication2/MyDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using WebApplication2.Entities;
+
+namespace WebApplication2
+{
+    public class MyDatabase:DbContext
+    {
+        public MyDatabase():base("name:InsightEntities")
+        {
+            //this.Configuration.LazyLoadingEnabled = false;
+            //this.Configuration.ProxyCreationEnabled = false;
+            //(this as IObjectContextAdapter).ObjectContext.CommandTimeout = 240;
+        }
+
+        public DbSet<TokenTest> TokenTest { get; set; }
+    }
+}
---
+++ WebApplication2/WebApplication2/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using WebApplication2.Entities;
+
+namespace WebApplication2
+{
+    public class UserRepository:IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TokenTest> FindUser(string userName, string password)
+        {
+            using (var db = new MyDatabase())
+            {
+                try
+                {
+                    return new TokenTest()
+                    {
+                        Password = "abc",
+                        UserName = "Farid",
+                        ConfirmPassword = "abc"
+                    };
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+              
+            }
+        }
+
+    }
+}
